794a23aaf8b7084702d4f5061e86a9cfe3f0ab5b
magcks <max@m9x.de>
2014-02-11 18:35:11 +0100

    Update imprint.tpl.php

--- templates/imprint.tpl.php
+++ templates/imprint.tpl.php
@@ -1,26 +1,12 @@
 			<h2>Verantwortlich f&uuml;r den Inhalt nach &sect; 6 Abs. 2 MDStV</h2>
 			<p>
-				Julian Erbeling<br />
-				Julian von B&uuml;low<br />
-				Sezgen Ipekoglu<br />
-				Manuel H&uuml;bner<br />
-				Gabriel H&uuml;bner<br />
+				<!-- insert admis here -->
 			</p>
 			<p>
 				<h3>Kontakt</h3>
-				Hans-B&ouml;ckler-Stra&szlig;e 2<br />
-				55128 Mainz<br />
-				Telefon: 06131 / 9931-0<br />
-				Telefax: 06131 / 364052<br />
-				eMail: <a href="mailto:info@igsmz.net">info@igsmz.net</a>
+				<!-- insert address here -->
 			</p>
 			<p>
-				<h3>Bei Problemen oder Fragen zum Online-Vertretungsplan</h3>
-				Julian Erbeling<br />
-				eMail: <a href="mailto:kontakt@julian-erbeling.de">kontakt@julian-erbeling.de</a>
-			</p>
-			<p>
-				<h3>Mitwirkende Entwickler</h3>
-				<div>&copy; 2012-2013 Max von Bülow</div>
-				<div>&copy; 2013 Julian Erbeling</div>
+				<h3>Frage, Sourcecode, etc.</h3>
+				<a href="https://github.com/magcks/vplan" target="_blank">Github Repositor</ya>
 			</p>
